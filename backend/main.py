2b29ca5afee262a86835bbf289bd216bd652d425
Liam <liam.wittig.hn@gmail.com>
2021-07-10 20:04:41 +0200

    main.py verbessert und neue Namen Funktion

--- backend/main.py
+++ backend/main.py
@@ -4,32 +4,51 @@ import json
 app = Flask(__name__)
 counterFragen = 0
 antworten = []
+
+nameLocal = ""
 def write_Json(antwortenLocal, name):
-    aList = [{name: antwortenLocal}]
-    jsonString = json.dumps(aList)
-    jsonFile = open("data.json", "w")
-    jsonFile.write(jsonString+"\n")
+    print("Write Json...")
+    print(antwortenLocal)
+    jsonFile = open("data.csv", "a")
+    jsonFile.write(str(name) + ",")
+    for i in antwortenLocal:
+        jsonFile.write(str(i) + ",")
+    jsonFile.write("\n")
     jsonFile.close()
 
+
 @app.route("/", methods=['GET', 'POST'])
-
-
 def index():
     global counterFragen
     global antworten
-
+    global nameLocal
     fragen = ["welchen Schultyp besuchst du? ", "Zu welcher Altergruppe gehörst du?", "Ich brauche Hilfe in..."
         , "Welcher Lerntyp bist du?", "Mein/e Hobbies sind eher...", "meine Stärken sind", "meine Schwächen sind"]
 
     print(counterFragen)
     fragenNummer = "Frage " + str(counterFragen + 1)
     if 0 == 0:
+        test2 = """<p>Name <input type = "text" name = "name" /></p>"""
+
         möglichkeiten = ["Gymnasium", "Realschule", "Hauptschule", "Gesammtschule"]
         test = """<input type="checkbox" id="3" name="hello" value="Frage 3">
                     <label for="3">""" + möglichkeiten[3] + """</label><br><br>"""
     print(request.form.getlist('hello'))
 
     if "Bestätigen" in request.form:
+
+        form_data = request.form.get("name")
+
+        if request.form.get("Field1_name") != "":
+            test2 = ""
+
+        if form_data is not None:
+            test2 = ""
+            if counterFragen ==0:
+                nameLocal = form_data
+            print("Name = ", form_data)
+
+        print(form_data)
         antwort = request.form.getlist('hello')
         print(int(antwort[0]))
         init = int(antwort[0])
@@ -62,10 +81,11 @@ def index():
             <label for="3">""" + möglichkeiten[3] + """</label>"""
         fragenNummer = "Frage " + str(counterFragen + 1)
         if counterFragen == 7:
-            write_Json(antworten, "hans")
+            print("Name2 = ", form_data)
+            write_Json(antworten, str(nameLocal))
 
     return render_template("index.html", Tittel=fragenNummer, content=fragen[counterFragen],
-                           möglichkeiten=möglichkeiten, test=test)
+                           möglichkeiten=möglichkeiten, test=test, test2=test2)
 
 
 if __name__ == "__main__":
